734276e986500658f1c4d8cf0550b84e0a9e13fd
Minq05 <hoangquan22022005@gmail.com>
2025-05-14 18:15:22 +0700

    Get by id Category and delete Category

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -35,3 +35,30 @@ export const getCategoriesAll = async (req, res) => {
     res.status(500).json({ message: "Lỗi server khi lấy danh mục", error });
   }
 };
+
+// Lấy danh mục theo ID
+export const getCategoryById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const category = await Category.findOne({ _id: id });
+    if (!category)
+      return res.status(404).json({ message: "Không tìm thấy danh mục" });
+    res.json(category);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Lỗi server khi lấy danh mục" });
+  }
+};
+
+// Xóa danh mục bằng findOneAndDelete
+export const deleteCategory = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleted = await Category.findOneAndDelete({ _id: id });
+    if (!deleted)
+      return res.status(404).json({ message: "Không tìm thấy danh mục" });
+    res.json({ message: "Xoá danh mục thành công" });
+  } catch (error) {
+    res.status(500).json({ message: "Xoá thất bại", error });
+  }
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { getCategoriesAll } from "../controllers/categoryController.js";
+import {
+  deleteCategory,
+  getCategoriesAll,
+  getCategoryById,
+} from "../controllers/categoryController.js";
 
 const routes = Router();
 
 // Routes category
 routes.get("/categories", getCategoriesAll);
+routes.get("/categories/:id", getCategoryById);
+routes.delete("/categories/:id", deleteCategory);
 
 export default routes;
