a1cfc72517b230c1a057c7bbd2e38d6d12c55a71
Minq05 <hoangquan22022005@gmail.com>
2025-05-16 15:25:20 +0700

    feature/deleteAuthor and getAuthorById

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -3,6 +3,7 @@ import {
   successResponse,
 } from "../middlewares/responseHandler.js";
 import Authors from "../model/Author.js";
+import Book from "../model/Book.js";
 
 export const getAuthors = async (req, res) => {
   const {
@@ -37,5 +38,43 @@ export const getAuthors = async (req, res) => {
     );
   } catch (error) {
     return errorResponse(res, "Lỗi server khi lấy danh sách tác giả");
+  }
+};
+
+export const getAuthorById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const author = await Authors.findOne({ _id: id });
+    if (!author) {
+      return errorResponse(res, "Không tìm thấy tác giả", 404);
+    }
+    return successResponse(res, { data: author }, "Lấy tác giả thành công");
+  } catch (error) {
+    return errorResponse(res, "Lỗi server khi lấy tác giả");
+  }
+};
+
+export const deleteAuthor = async (req, res) => {
+  const { id } = req.params;
+  try {
+    // Cập nhật Book
+    await Book.updateMany({ author_id: id }, { $set: { author: "" } });
+    // Xoá author
+    const deletedAuthor = await Authors.findOneAndDelete({ _id: id });
+    if (!deletedAuthor) {
+      return errorResponse(res, "Không tìm thấy tác giả", 404);
+    }
+    // danh mục không xác định
+    const unauthored = {
+      _id: null,
+      name: "Không xác định",
+    };
+    return successResponse(
+      res,
+      { author: unauthored },
+      "Xoá tác giả thành công"
+    );
+  } catch (error) {
+    return errorResponse(res, "Lỗi server khi xoá tác giả");
   }
 };
