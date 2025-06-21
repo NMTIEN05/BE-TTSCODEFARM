b1afce6910452db13e913cabf93ae46757532d98
unknown <nguyenmanhcuong17042005@gmail.com>
2025-06-21 15:19:10 +0700

    add and delete wishlist

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,7 @@ import orderCouponRouter from "../modules/OrderCoupon/orderCouponRoutes.js";
 import bookReviewRouter from "../modules/ProductReview/bookReviewRoutes.js";
 import cartRouter from "../modules/Cart/cartRoutes.js";
 import productVariantRouter from "../modules/ProductVariant/productVariantRoutes.js";
+import wishlistRouter from "../modules/WishList/wishlistRouter.js";
 
 const router = express.Router();
 
@@ -47,5 +48,8 @@ router.use("", cartRouter);
 
 // Product Variant routes
 router.use("/variants", productVariantRouter);
+
+// Wishlist routes
+router.use("/wishlist", wishlistRouter);
 
 export default router;
